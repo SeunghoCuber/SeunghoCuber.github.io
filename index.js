2e77bfa1d966e8c94ebfa58a57a1f7de08238f74
Seungho Lee <seungho9422@gmail.com>
2019-03-10 18:12:02 -0700

    commit message

---
+++ index.js
@@ -0,0 +1,35 @@
+var a = 10 % 8 * 3 / 2
+
+/*
+var arrayA = ["h", "b", "c", "d", "e", "f", "g"]
+
+arrayA.push("a")
+
+arrayA.sort()
+
+alert(arrayA)
+
+*/
+
+// var boo = false && true || !(true && true)
+// alert(boo)
+
+
+//alert(a==3)
+
+function addSquares(a, b) {
+	return(a * a + b * b);
+}
+
+function concat(s1, s2) {
+	return(s1 + s2);
+}
+
+function triProduct() {
+	alert("message sent");
+}
+
+
+function warning() {
+	alert("do not click me again!!!");
+}
